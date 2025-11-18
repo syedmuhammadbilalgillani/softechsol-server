6dbcc622ecf8099b762c490eada3564d538edf8b
Syed Muhammad Bilal Gillani <softechnologysolution@gmail.com>
2025-11-18 12:33:30 +0500

    Enhance Next.js configuration and update dependencies
    - Added PrismaPlugin to the Next.js webpack configuration for server-side builds in development mode.
    - Updated package.json and package-lock.json to include @prisma/nextjs-monorepo-workaround-plugin as a dev dependency.

--- next.config.ts
+++ next.config.ts
@@ -9,11 +9,15 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  outputFileTracingIncludes: {
-    "/api/**/*": ["./node_modules/.prisma/client/**/*"],
-
-    "/*": ["./node_modules/.prisma/client/**/*"],
-  },
 };
-
+// Add the plugin only for server-side builds
+if (process.env.NEXT_PUBLIC_NODE_ENV === "development") {
+  const { PrismaPlugin } = require("@prisma/nextjs-monorepo-workaround-plugin");
+  nextConfig.webpack = (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+    return config;
+  };
+}
 export default nextConfig;
